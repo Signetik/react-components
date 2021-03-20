15fffca7cd23d1e6cd55308fd8e488e97f26c87e
Steve Poulsen <spoulsen@signetik.com>
2021-03-20 17:03:58 -0500

    Login improvements.

--- Login/Login.js
+++ Login/Login.js
@@ -3,25 +3,23 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert"
 import MD5 from "crypto-js/md5";
+import { Redirect } from "react-router-dom"
+
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./Login.css";
 
-export default function Login() {
-  const [user, setUser] = useState("");
+export default function Login(props) {
   const [password, setPassword] = useState("");
   const [alert, setAlert] = useState("");
   const [alertVisible, setAlertVisible] = useState(false);
-  const [token, setToken] = useState("");
 
   function validateForm() {
-    return user.length > 0 && password.length > 0;
+    return props.user.length > 0 && password.length > 0;
   }
 
   function handleSubmit(event) {
     event.preventDefault();
-    setAlert("Failed to login")
-    setAlertVisible(true)
     setTimeout(() => {setAlertVisible(false)}, 5000)
 
     const requestOptions = {
@@ -30,12 +28,24 @@ export default function Login() {
       /*body: JSON.stringify({ fruit: 'Steve' })*/
     };
     const md5password = MD5(`${password}`).toString();
-    fetch(`http://10.10.10.4:8882/login?user=${user}&password=${md5password}`, requestOptions)
+    fetch(`http://10.10.10.4:8882/login?user=${props.user}&password=${md5password}`, requestOptions)
       .then(response => response.json())
       .then(data => {
         console.log(data)
-        setToken(data.token)
+        if (data.login === "success") {
+          props.setToken(data.token)
+        }
+        else {
+          throw "Invalid login"
+        }
+      }).catch((error) => {
+        setAlert('Failed to login, ' + error.toString())
+        setAlertVisible(true)
       });
+  }
+
+  if (props.token.length > 0) {
+    return (<Redirect to='/dashboard' />)
   }
 
   return (
@@ -45,8 +55,8 @@ export default function Login() {
         <Form.Label>User</Form.Label>
         <Form.Control
           autoFocus
-          value={user}
-          onChange={(e) => setUser(e.target.value)}
+          value={props.user}
+          onChange={(e) => props.setUser(e.target.value)}
         />
       </Form.Group>
       <Form.Group size="lg" controlId="password">
@@ -63,7 +73,6 @@ export default function Login() {
     </Form>
     <br />
     <Alert show={alertVisible} variant="warning">{alert}</Alert>
-    <Alert show={token.length > 0} variant="warning">{token}</Alert>
   </div>
   )
 }
