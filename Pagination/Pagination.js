45a6b75366da6c0235547235f23fe300c8809f72
Steve Poulsen <spoulsen@signetik.com>
2023-02-21 21:11:18 -0600

    Fixup pagination component.

--- Pagination/Pagination.js
+++ Pagination/Pagination.js
@@ -10,28 +10,41 @@ export function Pagination(props) {
         pagestart = 1;
     }
 
+    if (pageend > props.totalPages) {
+        pageend = props.totalPages;
+        pagestart = pageend - count;
+        if (pagestart < 1)
+            pagestart = 1;
+    }
+
+    console.log("currentPage: " + props.currentPage);
+    console.log("pagestart: " + pagestart);
+    console.log("pageend: " + pageend);
+
     var pagination = []
     pagination.push(<div data-page={props.currentPage} data-total-pages={props.totalPages} />);
 
     if (pagestart > 1) {
-        pagination.push(<a>[First]</a>);
+        pagination.push(<a onClick={() => props.setPage(1)}>[First]</a>);
     } else {
         pagination.push(<a disabled>[First]</a>);
     }
 
     for (var k = pagestart; k < pageend + 1; k++) {
-        if (k === parseInt(props.currentPage, 10)) {
-            pagination.push(<a disable>{k}</a>);
+        let pageNumber = k;
+        if (pageNumber === parseInt(props.currentPage, 10)) {
+            pagination.push(<a disable>[{pageNumber}]</a>);
         } else {
             if (k > parseInt(props.totalPages, 10))
-                pagination.push(<a disabled>{k}</a>);
+                pagination.push(<a disabled>{pageNumber}</a>);
             else
-                pagination.push(<a>{k}</a>);
+                pagination.push(<a key={pageNumber} onClick={() => props.setPage(pageNumber)}>{pageNumber}</a>);
         }
     }
+
     //pagination.append( " ... " );
     if (pageend < parseInt(props.totalPages,10)) {
-        pagination.push(<a>[Last]</a>);
+        pagination.push(<a onClick={() => props.setPage(props.totalPages)}>[Last]</a>);
     } else {
         pagination.push(<a disabled>[Last]</a>);
     }
