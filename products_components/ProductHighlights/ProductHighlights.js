a30ff57ff62463d089b68ee281dc21fc59cb48aa
Steve Poulsen <spoulsen@signetik.com>
2023-08-07 14:05:49 -0500

    More module CSS conversion.

--- products_components/ProductHighlights/ProductHighlights.js
+++ products_components/ProductHighlights/ProductHighlights.js
@@ -1,13 +1,13 @@
 import React from "react"
-import "./ProductHighlights.css"
+import styles from "./ProductHighlights.module.css"
 
 const ProductHighlights = ({ highlights }) => {
   return (
-    <section className="product-highlights">
-      <div className="container">
-        <div className="product-highlights__row">
+    <section className={styles["product-highlights"]}>
+      <div className={styles["container"]}>
+        <div className={styles["product-highlights__row"]}>
           {highlights.cols.map((col, index) => (
-            <div className="product-highlights__col" key={index}>
+            <div className={styles["product-highlights__col"]} key={index}>
               <h5>{col.title}</h5>
               <ul>
                 {col.items.map((item, iIndex) => (
@@ -17,7 +17,7 @@ const ProductHighlights = ({ highlights }) => {
             </div>
           ))}
         </div>
-        <div className="product-highlights__additional">
+        <div className={styles["product-highlights__additional"]}>
           <p>{highlights.additional}</p>
         </div>
       </div>
