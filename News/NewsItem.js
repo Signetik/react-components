d161d8bc75255e0d0b3045840bb53d56b36e3e76
Steve Poulsen <spoulsen@signetik.com>
2023-08-08 09:05:18 -0500

    Fix News to be more clean.

--- News/NewsItem.js
+++ News/NewsItem.js
@@ -7,9 +7,9 @@ const NewsItem = ({
   const textClassName = `news-text-${number}`
   return (
     <div className={styles["news-item"]}>
-        {
+        <div className={styles["newsimage-wrapper"]}>
             <img className={styles["newsimage-1"]} src={src} />
-        }
+          </div>
       <div className={`${styles["news-text"]} ${styles[textClassName]}`}>{title}</div>
       <div className={styles["news-text-long"]} >{text}</div>
     </div>
