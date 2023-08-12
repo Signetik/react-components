67d95aa58a7edba3e8495a94fb484bc5a49cc875
Steve Poulsen <spoulsen@signetik.com>
2023-08-12 12:01:29 -0500

    Revamp header and menu.

--- Layout/Header/Header.js
+++ Layout/Header/Header.js
@@ -1,18 +1,22 @@
-import React from "react"
+import React, { Fragment } from "react"
 //import LogoImg from "/images/Signetik Logo_Color_HZ_Registered.png"
+import TopMenu from "../TopMenu/TopMenu"
 
 import styles from "./Header.module.css"
 
 const Header = () => (
   <div className={styles["banner"]}>
     <img src="/images/Signetik Logo_Color_HZ_Registered.png" className={styles["logo-img"]} alt="Signetik" />
+    <div className={styles["rightside"]}>
     <div className={styles["infobar"]}>
       <div className={styles["phone"]}>402.421.2652</div>
       <div className={styles["vertbar"]} />
       <div className={styles["email"]}>info@signetik.com</div>
     </div>
+    <TopMenu />
+    </div>
       <div className={styles["bannerspace"]} />
   </div>
-)
+      )
 
 export default Header
